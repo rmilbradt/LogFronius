c976d0efe18f78be48b6b0e783a8815a622d05bb
Rafael <rmilbradt@gmail.com>
2015-06-08 11:01:05 -0300

    ok

--- src/br/ufsm/csi/beans/CommonInverterData.java
+++ src/br/ufsm/csi/beans/CommonInverterData.java
@@ -1,3 +1,5 @@
+package br.ufsm.csi.beans;
+
 import javax.json.*;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,9 +9,9 @@ import java.util.Map;
 /**
  * Created by 201420240 on 01/06/2015.
  */
-public class NowSensorData {
+public class CommonInverterData {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("http://200.132.36.179/solar_api/v1/GetSensorRealtimeData.cgi?Scope=Device&DeviceId=0&DataCollection=NowSensorData");
+        URL url = new URL("http://200.132.36.179/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceId=1&DataCollection=br.ufsm.csi.beans.CommonInverterData");
         InputStream is = url.openStream();
         JsonReader rdr = Json.createReader(is);
         JsonObject obj = rdr.readObject();
@@ -20,8 +22,6 @@ public class NowSensorData {
             JsonObject value = (JsonObject) result.getValue();
 
             if(value.getJsonNumber("Value") != null){
-
-
                 JsonNumber valor = value.getJsonNumber("Value");
                 String unidade = value.getString("Unit");
                 System.out.println(result.getKey() + ": " + valor + " " + unidade);
