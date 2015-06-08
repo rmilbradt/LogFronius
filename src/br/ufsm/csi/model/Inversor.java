3a01875ec353af504ecfdc571830184dbe67e2a7
Rafael <rmilbradt@gmail.com>
2015-06-08 10:41:59 -0300

    ok

---
+++ src/br/ufsm/csi/model/Inversor.java
@@ -0,0 +1,7 @@
+package br.ufsm.csi.model;
+
+/**
+ * Created by politecnico on 08/06/2015.
+ */
+public class Inversor {
+}
--- src/main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by 201420240 on 01/06/2015.
- */
-public class main {
-}
